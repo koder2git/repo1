f0e85a6f4a4fd0b349e0716ef78513632ef54514
koder2 <koder2@protonmail.com>
2020-07-02 21:19:23 +0300

    new Arabian to roman converter

---
+++ ConsoleApp1/ConsoleApp1/Converter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public partial class Converter : Component
+    {
+        static readonly int[] arabian = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        static readonly string[] roman = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+        public static void ArabianToRoman(int input)
+        {
+            int i = 0;
+            string str = "";
+            while (input > 0)
+            {
+                if (arabian[i] <= input)
+                {
+                    input -= arabian[i];
+                    str += roman[i];
+                }
+                else i++;
+            }
+            Console.Write("Roman number: ");
+            Console.WriteLine(str);
+        }
+
+        public static void RomanToArabian(string input)
+        {
+            int value = 0;
+            while (input.Length > 0)
+            {
+                foreach (var symb in roman)
+                {
+                    if (symb.Length > input.Length) continue;
+                    string sub = input.Substring(0, symb.Length);
+                    if (sub == symb)
+                    {
+                        value += arabian[Array.IndexOf(roman, symb)];
+                        input = input.Substring(symb.Length);
+                    }
+                }
+            }
+            Console.Write("Arabian number: ");
+            Console.WriteLine(value);
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intValue;
+
+            Console.WriteLine("Enter arabian or roman number: ");
+            
+            String strInput = Console.ReadLine();
+            
+            if (CheckIsSupported(strInput)) 
+            {
+                if (int.TryParse(strInput, out intValue))
+                {
+                    Converter.ArabianToRoman(intValue);
+                }
+                else
+                {
+                    Converter.RomanToArabian(strInput.ToUpper());
+                }
+            }
+            Console.ReadKey();
+
+            static bool IsSupported(string str)
+            {
+                if (str != "")
+                {
+                    Regex regex = new Regex(@"^(\d*|[IVXLCDMivxlcdm]*)$");
+                    return regex.IsMatch(str);
+                }
+                else return false;
+            }
+
+            static bool CheckIsSupported(string str)
+            {
+                if (IsSupported(str))
+                { return true; }
+                else
+                {
+                    Console.WriteLine("Unsupported input.");
+                    return false;
+                }
+            }
+        }
+    }
+}
