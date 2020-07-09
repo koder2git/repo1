f7efd3fb06a7482e5f84a1d85afd47aa34899314
koder2 <koder2@protonmail.com>
2020-07-09 23:30:06 +0300

    AaddItem and GetItemByType ready

---
+++ InetStoreAccounting/InetStoreAccounting/Item.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace InetStoreAccounting
+{
+    public abstract class Item
+    {
+        public Guid ID { get; private set; }
+        public string Name { get; private set; }
+        public int Amount { get; private set; }
+        public decimal Price { get; private set; }
+
+        public Item(Guid id, string name, int amount, decimal price)
+        {
+            ID = id;
+            if (String.IsNullOrWhiteSpace(name)) throw new ArgumentException("The name is required.");
+            Name = name;
+            if (amount <= 0) throw new ArgumentOutOfRangeException("The number of things cannot be zero or negative.");
+            Amount = amount;
+            if (price <= 0) throw new ArgumentOutOfRangeException("The price cannot be zero or negative.");
+            Price = price;
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine($"ID:\t\t\t{ID}");
+            Console.WriteLine($"Name:\t\t\t{Name}");
+            Console.WriteLine($"Amount:\t\t\t{Amount}");
+            Console.WriteLine($"Price $:\t\t{Price}");
+            Console.WriteLine($"Total price $:\t\t{Price * Amount}");
+        }
+    }
+
+    public class Phone : Item
+    {
+        public Phone(Guid id, string name, int amount, decimal price) : base(id, name, amount, price) { }
+    }
+
+    public class Computer : Item
+    {
+        public string Description { get; private set; }
+        public Computer(Guid id, string name, int amount, decimal price, string description) : base(id, name, amount, price)
+        {
+            Description = description;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine($"Description:\t\t{Description}");
+            base.Print();
+        }
+    }
+
+    class Clothes : Item
+    {
+        public string Size { get; private set; }
+        public Clothes(Guid id, string name, int amount, decimal price, string size) : base(id, name, amount, price)
+        {
+            Size = size;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine($"Size:\t\t{Size}");
+            base.Print();
+        }
+    }
+
+    class Car : Item
+    {
+        public int ProductionYear { get; private set; }
+        public Car(Guid id, string name, int amount, decimal price, int productionYear) : base(id, name, amount, price)
+        {
+            ProductionYear = productionYear;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine($"Year of producton:\t{ProductionYear}");
+            base.Print();
+        }
+    }
+}
--- InetStoreAccounting/InetStoreAccounting/Program.cs
+++ InetStoreAccounting/InetStoreAccounting/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Collections;
 
 namespace InetStoreAccounting
 {
@@ -8,19 +9,63 @@ namespace InetStoreAccounting
     {
         static void Main(string[] args)
         {
-
             Storage stor = new Storage();
-            stor.AddItem(new Car(Guid.NewGuid(), "shit", 1, 100500.00m, 1982));
-            stor.AddItem("Phone", "shit", 1, 100500.00m);
-            stor.PrintDic();
+            stor.AddItem(new Car(Guid.NewGuid(), "shit-car0", 1, 100500.00m, 1982));
+            stor.AddItem(new Car(Guid.NewGuid(), "shit-car1", 1, 100500.00m, 1982));
+            stor.AddItem(new Car(Guid.NewGuid(), "shit-car2", 1, 100500.00m, 1982));
+            stor.AddItem(new Car(Guid.NewGuid(), "shit-car3", 1, 100500.00m, 1982));
+            stor.AddItem(new Car(Guid.NewGuid(), "shit-car4", 1, 100500.00m, 1982));
+            stor.AddItem(new Phone(Guid.NewGuid(), "shit phone #1", 10, 100500.00m));
+            stor.AddItem(new Phone(Guid.NewGuid(), "shit phone #2", 10, 100500.00m));
+            stor.AddItem(new Phone(Guid.NewGuid(), "shit phone #3", 10, 100500.00m));
+            stor.AddItem(new Computer(Guid.NewGuid(), "shit_comp !", 99, 100500.00m, "PC"));
+            stor.AddItem(new Computer(Guid.NewGuid(), "shit_comp @", 99, 100500.00m, "PC"));
+            stor.AddItem(new Computer(Guid.NewGuid(), "shit_comp #", 99, 100500.00m, "PC"));
+            stor.AddItem(new Computer(Guid.NewGuid(), "shit_comp $", 99, 100500.00m, "PC"));
 
-            Dictionary<Guid, dynamic> dict = new Dictionary<Guid, dynamic>();
+            IEnumerable query = from item in stor.GetItemByType(typeof(Car)) where item.Name  == "shit-car2" select item;
+            foreach (Car item in query)
+            {
+                Console.WriteLine(item.ID);
+                Console.WriteLine(item.Name);
+                Console.WriteLine(item.ProductionYear);
+                Console.WriteLine(item.Amount);
+                Console.WriteLine(item.Price);
+                Console.WriteLine();
+                Console.ReadKey();
+            }
 
-            dict.Add(Guid.NewGuid(), new Phone(Guid.NewGuid(), "0sucks", 1, 100501.01m));
-            dict.Add(Guid.NewGuid(), new Phone(Guid.NewGuid(), "1sucks", 1, 100501.01m));
-            dict.Add(Guid.NewGuid(), new Car(Guid.NewGuid(), "shit", 1, 100500.00m, 1982));
-            dict.Add(Guid.NewGuid(), new Phone(Guid.NewGuid(), "2sucks", 1, 100501.01m));
-            dict.Add(Guid.NewGuid(), new Phone(Guid.NewGuid(), "3sucks", 1, 100501.01m));
+            foreach (var item in stor.GetItemByType(typeof(Car)))
+            {
+                item.Print();
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+
+            foreach (var item in stor.GetItemByType(typeof(Computer)))
+            {
+                item.Print();
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+            
+            foreach (var item in stor.GetItemByType(typeof(Phone)))
+            {
+                item.Print();
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+
+            //stor.AddItem("Phone", "shit", 1, 100500.00m);
+            //stor.PrintDic();
+
+            //Dictionary<Guid, dynamic> dict = new Dictionary<Guid, dynamic>();
+
+            //dict.Add(Guid.NewGuid(), new Phone(Guid.NewGuid(), "0sucks", 1, 100501.01m));
+            //dict.Add(Guid.NewGuid(), new Phone(Guid.NewGuid(), "1sucks", 1, 100501.01m));
+            //dict.Add(Guid.NewGuid(), new Car(Guid.NewGuid(), "shit", 1, 100500.00m, 1982));
+            //dict.Add(Guid.NewGuid(), new Phone(Guid.NewGuid(), "2sucks", 1, 100501.01m));
+            //dict.Add(Guid.NewGuid(), new Phone(Guid.NewGuid(), "3sucks", 1, 100501.01m));
 
             //foreach (KeyValuePair<Guid, dynamic> pair in dict)
             //{
@@ -28,37 +73,37 @@ namespace InetStoreAccounting
             //    Console.WriteLine(pair.Value.GetType().Name);
             //}
 
-            var query = from item in dict where item.Value.GetType().Name == "Car" select item;
-            foreach (var item in query)
-            {
-                Console.WriteLine(item.Value.Name);
-            }
-            Console.ReadKey();
+            //var query = from item in dict where item.Value.GetType().Name == "Car" select item;
+            //foreach (var item in query)
+            //{
+            //    Console.WriteLine(item.Value.Name);
+            //}
+            //Console.ReadKey();
 
-            Car car = new Car(Guid.NewGuid(), "shit", 1, 100500.00m, 1982);
+            //Car car = new Car(Guid.NewGuid(), "shit", 1, 100500.00m, 1982);
 
-            List<Phone> phones = new List<Phone>();
-            phones.Add(new Phone(Guid.NewGuid(), "0sucks", 1, 100501.01m));
-            phones.Add(new Phone(Guid.NewGuid(), "1sucks", 10, 100502.01m));
-            phones.Add(new Phone(Guid.NewGuid(), "2sucks", 100, 100503.01m));
-            phones.Add(new Phone(Guid.NewGuid(), "3sucks", 1000, 100504.01m));
+            //List<Phone> phones = new List<Phone>();
+            //phones.Add(new Phone(Guid.NewGuid(), "0sucks", 1, 100501.01m));
+            //phones.Add(new Phone(Guid.NewGuid(), "1sucks", 10, 100502.01m));
+            //phones.Add(new Phone(Guid.NewGuid(), "2sucks", 100, 100503.01m));
+            //phones.Add(new Phone(Guid.NewGuid(), "3sucks", 1000, 100504.01m));
 
-            foreach (var phone in phones)
-            {
-                Console.WriteLine($"Phone ID:\t\t{phone.ID}");
-                Console.WriteLine($"Phone moder:\t\t{phone.Name}");
-                Console.WriteLine($"Amount of phone(s):\t{phone.Amount}");
-                Console.WriteLine($"Price $:\t\t{phone.Price}");
-                Console.WriteLine($"Total price $:\t\t{phone.Price * phone.Amount}");
-                Console.WriteLine();
-            }
-            
-            Console.WriteLine($"Car IDr:\t\t{car.ID}");
-            Console.WriteLine($"Car moder:\t\t{car.Name}");
-            Console.WriteLine($"Year of production:\t{car.ProductionYear}");
-            Console.WriteLine($"Amount of car(s):\t{car.Amount}");
-            Console.WriteLine($"Price $:\t\t{car.Price}");
-            Console.ReadKey();
+            //foreach (var phone in phones)
+            //{
+            //    Console.WriteLine($"Phone ID:\t\t{phone.ID}");
+            //    Console.WriteLine($"Phone moder:\t\t{phone.Name}");
+            //    Console.WriteLine($"Amount of phone(s):\t{phone.Amount}");
+            //    Console.WriteLine($"Price $:\t\t{phone.Price}");
+            //    Console.WriteLine($"Total price $:\t\t{phone.Price * phone.Amount}");
+            //    Console.WriteLine();
+            //}
+
+            //Console.WriteLine($"Car IDr:\t\t{car.ID}");
+            //Console.WriteLine($"Car moder:\t\t{car.Name}");
+            //Console.WriteLine($"Year of production:\t{car.ProductionYear}");
+            //Console.WriteLine($"Amount of car(s):\t{car.Amount}");
+            //Console.WriteLine($"Price $:\t\t{car.Price}");
+            //Console.ReadKey();
         }
     }
 }
